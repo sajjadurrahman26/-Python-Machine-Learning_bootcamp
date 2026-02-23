d0605f221c785e91ed990b6788fd5046dcf3d54c
Kazi Sajjadur Rahman <sajjadrahman31@gmail.com>
2026-02-23 09:56:22 +0600

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,19 @@
+**Question 2: Operators**
+
+a = 17
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+
+print("Power:", a ** b)
+
+#Comparison results
+print("17 > 5:", a > b)
+print("17 == 5:", a == b)
+print("17 != 5:", a != b)
