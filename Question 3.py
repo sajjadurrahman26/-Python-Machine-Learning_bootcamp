77f544ace7b723d8b1c59b79d532e279194167f8
Kazi Sajjadur Rahman <sajjadrahman31@gmail.com>
2026-02-23 09:57:14 +0600

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,15 @@
+**Question 3: String Manipulation**
+
+name = "python programming"
+
+print(name.upper())
+print(name.title())
+print(name.replace("programming", "language"))
+print(len(name))
+
+name = "Sajjadur Rahman"
+age = 24
+gpa = 3.74
+
+#Using an f-string
+print(f"My name is {name}, I am {age} years old, and my GPA is {gpa}")
